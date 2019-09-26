164f12f865639cdc60ed7acce4883e24106c8594
Jawaria Murtaza <jawariamurtaza786@gmail.com>
2019-09-26 21:57:07 +0500

    my first commit

---
+++ first.py
@@ -0,0 +1,14 @@
+print ('Hello World')
+print ("Hello World")
+print "Jhon"
+print "Jhon Kennedy"
+print "Hi, I am Python."
+print 25
+print 2*2
+print 2 + 5 + 9
+print "Hello,","I","am","Jhon"
+print "Jhon","is",20,"years","old"
+print "Hello Jhon," + "How are you?"
+print "I am 20 years old " + "and" + "My friend is 21 years old."
+print "20" + "+" + "2" "=" + "22"
+print "My brother is" + 25 + " years old."
